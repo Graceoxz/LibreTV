2505b2d7810b9f00e7d811dbd0a34dfa36720575
GitHub Action <action@github.com>
2025-10-06 03:34:47 +0000

    🔄 Sync code from upstream

--- js/customer_site.js
+++ js/customer_site.js
@@ -2,31 +2,6 @@ const CUSTOMER_SITES = {
     qiqi: {
         api: 'https://www.qiqidys.com/api.php/provide/vod',
         name: '七七资源',
-        api: 'http://sdzyapi.com/api.php/provide/vod/from/sdm3u8',
-        name: '红牛资源',
-        api: 'http://collect.wolongzyw.com/api.php/provide/vod',
-        name: '卧龙资源',
-        api: 'http://sdzyapi.com/api.php/provide/vod/from/sdm3u8',
-        name: '闪电资源',
-        api: 'http://sdzyapi.com/api.php/provide/vod/from/sdm3u8',
-        name: '红牛资源',
-        api: 'http://sdzyapi.com/api.php/provide/vod/from/sdm3u8',
-        name: '红牛资源',
-        api: 'http://sdzyapi.com/api.php/provide/vod/from/sdm3u8',
-        name: '红牛资源',
-        api: 'http://sdzyapi.com/api.php/provide/vod/from/sdm3u8',
-        name: '红牛资源',
-        api: 'http://sdzyapi.com/api.php/provide/vod/from/sdm3u8',
-        name: '红牛资源',
-        api: 'http://sdzyapi.com/api.php/provide/vod/from/sdm3u8',
-        name: '红牛资源',
-        api: 'http://sdzyapi.com/api.php/provide/vod/from/sdm3u8',
-        name: '红牛资源',
-        api: 'http://sdzyapi.com/api.php/provide/vod/from/sdm3u8',
-        name: '红牛资源',
-        api: 'http://sdzyapi.com/api.php/provide/vod/from/sdm3u8',
-        name: '红牛资源',
-        
     }
 };
 
